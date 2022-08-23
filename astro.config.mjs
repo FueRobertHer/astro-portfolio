cbc2fc29bb86264befae31b274f3295189c9ea78
Fue <fue@remesh.org>
2022-08-23 15:27:36 -0700

    Update astro config for deloy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://fueroberther.github.io",
+  base: "/astro-portfolio",
 });
