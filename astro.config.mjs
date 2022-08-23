cd53afcc950bed68fdf2d8eb93c3a6343b4cfa58
Fue <fue@remesh.org>
2022-08-23 15:45:23 -0700

    fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://fueroberther.github.io",
-  base: "/astro-portfolio",
 });
