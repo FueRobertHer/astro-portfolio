553754f8095fd6a0f642b381c3fb672e6528b22b
Fue <fue@remesh.org>
2022-08-26 09:55:58 -0700

    Add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://fueroberther.github.io",
-  base: "/astro-portfolio",
+  output: "server",
+  adapter: vercel(),
 });
