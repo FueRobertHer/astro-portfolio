8b057e380460562182388b675077a31481f03e47
FueRobertHer <fueroberther@gmail.com>
2024-03-29 02:15:45 -0700

    static build

--- .vercel/output/functions/_render.func/home/fue/repo/astro-portfolio/.vercel/output/_functions/chunks/projects_BNZBsEXN.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/projects_CBc1GU9K.mjs');
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
+  output: "static",
   adapter: vercel(),
 });
